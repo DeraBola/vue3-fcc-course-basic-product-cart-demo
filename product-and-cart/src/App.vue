3340e02221ee888ffaa3389be64e135b71c37951
DeraBola <96840776+DeraBola@users.noreply.github.com>
2022-06-16 13:28:17 +0100

    commit

--- product-and-cart/src/App.vue
+++ product-and-cart/src/App.vue
@@ -2,21 +2,21 @@
 
 <header class="top-bar spread">
             <nav class="top-bar-nav">
-                <router-link href="#" class="top-bar-link">
+                <router-link to="/" class="top-bar-link">
                     <i class="icofont-spoon-and-fork"></i>
                     <span>Home</span>
                 </router-link>
-                <router-link href="views/products.html" class="top-bar-link">
+                <router-link to="/products" class="top-bar-link">
                     <span>Products</span>
                 </router-link>
-                <router-link href="views/past-orders.html" class="top-bar-link">
+                <router-link to="/past-orders" class="top-bar-link">
                     <span>Past Orders</span>
                 </router-link>
             </nav>
-            <router-link @click="toggleSidebar" class="top-bar-cart-link">
+          <!--  <router-link @click="toggleSidebar" class="top-bar-cart-link">
                 <i class="icofont-cart-alt icofont-1x"></i>
                 <span>Cart ({{totalQuantity}})</span>
-            </router-link>
+            </router-link>  -->
         </header>
   <router-view/>
 </template>
