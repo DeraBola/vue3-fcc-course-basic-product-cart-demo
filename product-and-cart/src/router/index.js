715ed3f7f2b7ac3e8270e3760810587b78b5a2d2
DeraBola <96840776+DeraBola@users.noreply.github.com>
2022-06-16 13:41:35 +0100

    commit

--- product-and-cart/src/router/index.js
+++ product-and-cart/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
+import Products from '../views/Products.vue'
 
 const routes = [{
         path: '/',
@@ -8,9 +8,9 @@ const routes = [{
         component: HomeView
     },
     {
-        path: '/about',
-        name: 'about',
-        component: AboutView
+        path: '/products',
+        name: 'Products',
+        component: Products
     }
 ]
 
